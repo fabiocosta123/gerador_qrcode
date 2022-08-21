8bf3f6f8460ac55a1955520951c495dc5465201d
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-08-21 16:54:57 -0300

    instalação do pacote react-qr-code, e qrcode, criando estrutura e estilizando no app.css, criando função para inserção de dados dinâmicos usando value

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
+
+import QRcode from "react-qr-code";
+import QRcodeLink from "qrcode";
 
 function App() {
+  const [link, setLink] = useState("");
+  const [qrcodeLink, setQrcodeLink] = useState("");
+
+  function handleGenerate(link_url) {
+    QRcodeLink.toDataURL(
+      link_url,
+      {
+        width: 600,
+        margin: 3,
+      },
+      function (err, url) {
+        setQrcodeLink(url);
+      }
+    );
+  }
+
+  function handleQrcode(e) {
+    setLink(e.target.value);
+    handleGenerate(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Gerador de Qrcode</h1>
+
+      <QRcode value={link} />
+
+      <input
+        placeholder="Digite seu link"
+        className="input"
+        value={link}
+        onChange={(e) => handleQrcode(e)}
+      />
+
+      <a href={qrcodeLink} download={`qrcode.png`}>
+        Baixar QrCode
+      </a>
     </div>
   );
 }
